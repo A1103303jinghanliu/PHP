2e4a4dbffc772b826999e294cdddcebcaece6f58
A1103303jinghanliu <125644656+A1103303jinghanliu@users.noreply.github.com>
2023-02-20 19:14:28 +0800

    Add files via upload

---
+++ FirstPHP.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>A1103303 FirstPHP</title>
+</head>
+<body>
+<?php
+
+$n=6;
+for($i=1; $i<=$n; $i++){
+    for($j=$i; $j>=1; $j--){
+        echo $j;
+    }
+    echo '<br>';
+}
+
+
+?>
+</body>
+</html>
